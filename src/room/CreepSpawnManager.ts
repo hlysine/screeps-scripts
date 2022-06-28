5775c95d678df1acf130ce136980e4c086459250
Henry Lin <henry.ys.lin@gmail.com>
2022-06-28 15:26:38 +0800

    Improve CreepSpawnManager with cached spawn info and dynamic creep limit

--- src/room/CreepSpawnManager.ts
+++ src/room/CreepSpawnManager.ts
@@ -4,6 +4,7 @@ import { ActionType } from "creep/Action";
 interface SpawnInfo {
   spawn: StructureSpawn;
   bodyParts: BodyPartConstant[];
+  creepLimit: number;
 }
 
 function getBodyParts(energyCapacity: number): BodyPartConstant[] {
@@ -25,30 +26,44 @@ function getBodyParts(energyCapacity: number): BodyPartConstant[] {
   return bodyParts;
 }
 
-class CreepSpawnManager implements Manager {
-  public creepsPerSpawn = 15;
+function getCreepLimit(room: Room): number {
+  let limit = 1;
+  if (room.controller) {
+    limit += room.controller.level + 3;
+  }
+  limit += Object.values(Game.structures).filter(s => s.pos.roomName === room.name).length;
+  return limit;
+}
 
-  public constructor() {
-    console.log("CreepSpawnManager init");
+class CreepSpawnManager implements Manager {
+  public spawns: SpawnInfo[] | undefined;
+
+  public spawnRefreshFrequency = 10;
+
+  public getSpawns(): SpawnInfo[] {
+    return Object.values(Game.spawns).map(spawn => {
+      const energyAvailable = spawn.room.energyCapacityAvailable;
+      return {
+        spawn,
+        bodyParts: getBodyParts(energyAvailable),
+        creepLimit: getCreepLimit(spawn.room)
+      };
+    });
   }
 
   public loop(): void {
     const creeps = Object.values(Game.creeps);
     console.log(`Total creeps: ${creeps.length}`);
 
-    const spawns: SpawnInfo[] = [];
-    Object.values(Game.spawns).forEach(spawn => {
-      const energyAvailable = spawn.room.energyCapacityAvailable;
-      spawns.push({
-        spawn,
-        bodyParts: getBodyParts(energyAvailable)
-      });
-    });
+    if (!this.spawns || Game.time % this.spawnRefreshFrequency === 0) {
+      if (!this.spawns) console.log("Initializing spawn info");
+      this.spawns = this.getSpawns();
+    }
 
-    spawns.forEach(spawnInfo => {
+    this.spawns.forEach(spawnInfo => {
       const { spawn, bodyParts } = spawnInfo;
       const creepsInSpawn = creeps.filter(c => c.room.name === spawn.room.name);
-      if (creepsInSpawn.length < this.creepsPerSpawn) {
+      if (creepsInSpawn.length < spawnInfo.creepLimit) {
         const newName = `${spawn.room.name}-Creep${Game.time}`;
         const result = spawn.spawnCreep(bodyParts, newName, {
           memory: { action: ActionType.Idle }
