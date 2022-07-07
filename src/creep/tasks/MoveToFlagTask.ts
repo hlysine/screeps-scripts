7c088fdf4d03121ac6bf8642579f76fb3c2a8b1b
Henry Lin <henry.ys.lin@gmail.com>
2022-07-07 08:58:58 +0800

    Fix MoveToFlagTask not working

--- src/creep/tasks/MoveToFlagTask.ts
+++ src/creep/tasks/MoveToFlagTask.ts
@@ -7,15 +7,15 @@ export enum MoveToFlagMode {
   /**
    * Keep moving until within a certain range of the flag.
    */
-  Range,
+  Range = "range",
   /**
    * Complete the task once in the same room as the flag.
    */
-  RoomOnly,
+  RoomOnly = "roomOnly",
   /**
    * Switch to a background task once in the same room as the flag.
    */
-  LowPriorityInRoom
+  LowPriorityInRoom = "lowPriorityInRoom"
 }
 
 export default function MoveToFlagTask(mode: MoveToFlagMode, range: number): Task {
@@ -26,35 +26,42 @@ export default function MoveToFlagTask(mode: MoveToFlagMode, range: number): Tas
 
     steps: [
       (creep, ctx, next) => {
-        const target = FlagManager.getRelatedFlags(creep.memory.origin).find(f =>
+        const target = FlagManager.getRelatedFlags(Game.spawns[creep.memory.origin]?.pos.roomName ?? "").find(f =>
           f.name.toLowerCase().includes("@" + creep.memory.role)
         );
         if (!target) {
-          next();
+          ctx.note = "No target";
+          ctx.status = TaskStatus.Complete;
           return;
         }
         if (mode === MoveToFlagMode.Range || mode === MoveToFlagMode.LowPriorityInRoom) {
           if (creep.pos.roomName === target.pos.roomName && creep.pos.inRangeTo(target, range)) {
-            next();
+            ctx.note = mode + " goal reached";
+            ctx.status = TaskStatus.Complete;
             return;
           }
         } else if (mode === MoveToFlagMode.RoomOnly) {
           if (creep.pos.roomName === target.pos.roomName) {
-            next();
+            ctx.note = mode + " goal reached";
+            ctx.status = TaskStatus.Complete;
             return;
           }
         }
         if (isMoveSuccess(creep.moveTo(target.pos, { visualizePathStyle: { stroke: "#ffffff" }, range }))) {
           if (creep.pos.roomName === target.pos.roomName) {
             if (mode === MoveToFlagMode.LowPriorityInRoom) {
+              ctx.note = mode + ": already in room";
               ctx.status = TaskStatus.Background;
             } else if (mode === MoveToFlagMode.RoomOnly) {
+              ctx.note = mode + " goal reached while moving";
               ctx.status = TaskStatus.Complete;
             } else {
+              ctx.note = mode + ": in progress";
               ctx.status = TaskStatus.InProgress;
             }
             return;
           } else {
+            ctx.note = "moving to room";
             ctx.status = TaskStatus.InProgress;
             return;
           }
