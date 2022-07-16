18507d94fd4e4067683b43f518bcaa009faa9c1e
Henry Lin <henry.ys.lin@gmail.com>
2022-07-16 12:15:29 +0800

    Make the divider longer

--- src/main.ts
+++ src/main.ts
@@ -66,7 +66,7 @@ global.managers = managers;
 // When compiling TS to JS and bundling with rollup, the line numbers and file names in error messages change
 // This utility uses source maps to get the line numbers and file names of the original, TS source code
 export const loop = ErrorMapper.wrapLoop(() => {
-  console.log(`\n===== Current game tick is ${Game.time} =====\n`);
+  console.log(`\n==================== Current game tick is ${Game.time} ====================\n`);
 
   // Automatically delete memory of missing creeps
   for (const name in Memory.creeps) {
